26221cfb6c5b66ad04f1063dd03be9935b20321c
mariancristu <mariancristu9@gmail.com>
2020-02-13 17:40:11 -0600

    :construction: Add new methods
    Added my_any? and my_none?

--- enumerable.rb
+++ enumerable.rb
@@ -46,11 +46,70 @@ module Enumerable
     temp_h = {}
     is_2darr = false
     is_2darr = true unless self.class == Array
-    if is_2darr
-      my_each { |x, y| temp_h[x] = y if yield(x, y) }
-    else
-      my_each { |x| temp_a << x if yield(x) }
-    end
+    is_2darr == true ? my_each { |x, y| temp_h[x] = y if yield(x, y) } : my_each { |x| temp_a << x if yield(x) }
     is_2darr == false ? temp_a : temp_h
+  end
+
+  def my_any?(*datatype)
+    exproc = create_block
+    for_any(exproc, datatype, &exproc)
+  end
+
+  def my_none?(*datatype)
+    exproc = create_block
+    for_any(exproc, datatype, &exproc) == false
+  end
+
+  protected
+
+  def for_any(prc, datatype)
+    is_2darr = false
+    is_2darr = true unless self.class == Array
+    temp_response = check_if(is_2darr, self, datatype, &prc)
+    temp_response
+  end
+
+  def check_if(is_2d, obj, datatype)
+    tmp_res = false
+    if is_2d
+      obj.my_each do |x, y|
+        if check_dtype(datatype)
+          res = datatype[0].match(y.to_s) || datatype[0].match(x.to_s)
+          tmp_res = true if res != nil
+        elsif !datatype.empty?
+          tmp_res = y.is_a? datatype[0]
+          break if tmp_res
+        else
+          tmp_res = true if yield(x, y)
+        end
+      end
+    else
+      obj.my_each do |x|
+        if check_dtype(datatype)
+          res = datatype[0].match(x.to_s)
+          tmp_res = true if res != nil
+        elsif !datatype.empty?
+          tmp_res = x.is_a? datatype[0]
+          break if tmp_res
+        else
+          tmp_res = true if yield(x)
+        end
+      end
+    end
+    tmp_res
+  end
+
+  def create_block
+    oexproc = proc do |x, y|
+      yield(x, y)
+    end
+    twoexproc = proc do |x|
+      x
+    end
+    block_given? ? oexproc : twoexproc
+  end
+
+  def check_dtype(value)
+    value[0].class == Regexp
   end
 end
